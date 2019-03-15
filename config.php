d1a11a902b7909b574cb578a0c752b878b43fa6e
SLDux <47551368+SLDux@users.noreply.github.com>
2019-03-16 00:23:14 +1100

    wk4 done

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "week4_tuesday"; //will use later
+    $dsn = "mysql:host=$host;dbname=$dbname"; //will use later
+    $options = array(
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                );
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
